4b06327e66537036ba635b907426196886921098
Rajat947 <choudharyrajat1311@gmail.com>
2021-05-27 11:04:30 +0530

    Improved SQL query

--- demonstrate.py
+++ demonstrate.py
@@ -12,15 +12,10 @@ def authorize(user_id, password):
     except sqlite3.Error as error:
         print("Failed to connect..")
         exit()
-    sql_statement = """  SELECT * FROM account_info """ 
-    cursor.execute(sql_statement)
-    records = cursor.fetchall()
-    flag = 0
-    for row in records:
-        if user_id == row[0]:
-            if password == row[1]:
-                flag = 1  
-    if(flag):
+    # sql_statement =
+    cursor.execute('SELECT ID FROM account_info where ID=? AND password=?', (user_id, password))
+    records = cursor.fetchone() 
+    if(records != None):
         return user_id
     else:
         return None
@@ -28,5 +23,6 @@ def authorize(user_id, password):
 value = authorize(entered_userid, entered_pass)
 if value != None:
     print('User is Authorized')
+    print('Welcome back',value+'!')
 else:
     print('User Not Authorized')
